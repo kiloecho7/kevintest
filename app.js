3614c32028cf6d5d0942d3c179b07b0136636510
Kevin Edwards <kevin.edwards@octanner.com>
2017-01-07 19:19:03 -0700

    x

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express');
 // var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/routes');
+var index = require('./routes/index');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
